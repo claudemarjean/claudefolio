cd79320528071c7eaa9b6c918a1e03962da39f0c
c109 <marjean.8888@gmail.com>
2025-07-16 23:34:46 +0300

    modification data utils

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Licences",
+    duration: "2019 - 2020",
+    institution: "Ecole Nationale D'Informatique",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Master 2",
+    duration: "2021 - 2022",
+    institution: "Ecole Nationale D'Informatique",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,44 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Directeur Adjoint au Développement',
+    company: "MANAO Logiciels.",
+    duration: "(Jan 2024 - Présent)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Responsable Informatique",
+    company: "AFAMITA (alternance)",
+    duration: "(Nov 2020 - Présent)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Webmaster/Développeur",
+    company: "AB Airs Support (freelance)",
+    duration: "(Mars 2025)"
+  },
+  {
+    id: 4,
+    title: "Auditeur en assistance du SI",
+    company: "-",
+    duration: "(Décembre 2024)"
+  },
+  {
+    id: 5,
+    title: "Webmaster/Développeur",
+    company: "ExtraToo (freelance)",
+    duration: "( Juillet 2024 - Août 2024)"
+  },
+  {
+    id: 6,
+    title: "Chef de projet en développement",
+    company: "MANAO Logiciels",
+    duration: "( Juillet 2023 - Janvier 2024)"
+  },
+  {
+    id: 7,
+    title: "Développeur Web Full Stack (PHP-JS)",
+    company: "MANAO Logiciels",
+    duration: "( Avril 2022  - Juin 2023)"
   }
 ]
